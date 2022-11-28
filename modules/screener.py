ad4411592d3440f8a06df5ea765af1ab1c8f848a
Serhad Aras <serhad.aras@student.ap.be>
2022-11-28 21:31:15 +0100

    update

--- modules/screener.py
+++ modules/screener.py
@@ -12,12 +12,11 @@ def run(**args):
         sct_img = sct.grab(monitor)
         # Generate the PNG
         png = mss.tools.to_png(sct_img.rgb, sct_img.size)
-        print(png)
         #
         # # Save png to file
         with open('screenshot.png', 'wb') as f:
             f.write(png)
         # # Save png to file
-        with open(png, "rb") as image:
+        with open("screenshot.png", "rb") as image:
             encoded_string = base64.b64encode(image)
     return(encoded_string)
