60f7797a99a9c82e6e6504eea90613934cae31e7
Serhad Aras <serhad.aras@student.ap.be>
2022-11-28 20:31:01 +0100

    update

--- modules/screener.py
+++ modules/screener.py
@@ -1,20 +1,14 @@
 import mss
 import mss.tools
 
+def run(**args):
+    with mss.mss() as sct:
+        # The monitor or screen part to capture
+        monitor = sct.monitors[1]  # or a region
 
-with mss.mss() as sct:
-    # The monitor or screen part to capture
-    monitor = sct.monitors[1]  # or a region
+        # Grab the data
+        sct_img = sct.grab(monitor)
 
-    # Grab the data
-    sct_img = sct.grab(monitor)
-
-    # Generate the PNG
-    png = mss.tools.to_png(sct_img.rgb, sct_img.size)
-    # open image
-    with open('screenshot.png', 'wb') as f:
-        f.write(png)
-        
-
-    # Save to the picture file
-    print(sct_img)
+        # Generate the PNG
+        png = mss.tools.to_png(sct_img.rgb, sct_img.size)
+    return(png)
