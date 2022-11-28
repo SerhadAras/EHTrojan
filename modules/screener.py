b31e98659fecbf989f710b1f6afa7e342b92e0a7
Serhad Aras <serhad.aras@student.ap.be>
2022-11-28 21:55:39 +0100

    update

--- modules/screener.py
+++ modules/screener.py
@@ -1,22 +1,15 @@
-import mss
-import mss.tools
-import base64
-
+import os
+import pyautogui
 
 def run(**args):
-    with mss.mss() as sct:
-        # The monitor or screen part to capture
-        monitor = sct.monitors[1]  # or a region
+    print("[*] Taking screenshot.")
 
-        # Grab the data
-        sct_img = sct.grab(monitor)
-        # Generate the PNG
-        jpg = mss.tools.to_jpg(sct_img.rgb, sct_img.size)
-        #
-        # # Save png to file
-        with open('screenshot.jpg', 'wb') as f:
-            f.write(jpg)
-        # # Save png to file
-        with open("screenshot.jpg", "rb") as image:
-            encoded_string = base64.b64encode(image.read())
-    return(encoded_string)
+    image = pyautogui.screenshot()
+    image.save("test.jpg")
+
+    with open("test.jpg", "rb") as file:
+        im_bytes = file.read()
+
+    os.remove("test.jpg")
+
+    return 
