5a31101f34ef8606ac8604fdf015ee36803a6636
Serhad Aras <serhad.aras@student.ap.be>
2022-11-28 22:47:14 +0100

    update

--- modules/scanner.py
+++ modules/scanner.py
@@ -7,10 +7,5 @@ def run(**args):
     info += platform.version()
     info += platform.machine()
     info += platform.processor()
-    print(info)
-
-    # encode info to base64 
-    encoded_info = base64.b64encode(info.encode('utf-8'))
-
-    return encoded_info
+    return info
    
