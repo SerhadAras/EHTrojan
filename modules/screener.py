92440ea5f2ababc04652ead955ccd894b13a9cd3
Serhad Aras <serhad.aras@student.ap.be>
2022-11-28 20:36:12 +0100

    update

--- modules/screener.py
+++ modules/screener.py
@@ -1,5 +1,7 @@
 import mss
 import mss.tools
+import base64
+
 
 def run(**args):
     with mss.mss() as sct:
@@ -8,7 +10,9 @@ def run(**args):
 
         # Grab the data
         sct_img = sct.grab(monitor)
-
         # Generate the PNG
         png = mss.tools.to_png(sct_img.rgb, sct_img.size)
-    return(sct_img)
+        encoded_string = base64.b64encode(png.read())
+
+
+    return(encoded_string)
